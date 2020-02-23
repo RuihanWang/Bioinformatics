b3b4e653a21965e894fcba13b1c99c04c4c12c99
wangruihan <wang.ruih@husky.neu.edu>
2020-02-22 22:18:08 -0500

    MSA

---
+++ MSA/clustalo.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+#clustalo.sh
+
+clustalo -i apoe_aa.fasta -o clustal_result.fasta
--- MSA/translate_APOE.py
+++ MSA/translate_APOE.py
@@ -2,4 +2,16 @@
 #! translate_APOE.py
 
 
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+from Bio import SeqIO
+
+
+seq = SeqIO.parse("APOE_refseq_transcript.fasta","fasta")
+se = []
+for s in seq:
+    s.seq = s.seq.translate()
+    se.append(s)
+
+SeqIO.write(se,"apoe_aa.fasta","fasta")
 
